837f3d21a2c938e57b77646f41a3a2793aeee8cf
mdvonavi <mdvonavi@gmail.com>
2020-01-29 22:06:21 +0300

    реализовано скачивание и передача пользователю конкретной книги

--- flibusta_parsing.py
+++ flibusta_parsing.py
@@ -1,11 +1,18 @@
 import csv
-import urllib.request
 import re
 import urllib.parse
 import requests
 from bs4 import BeautifulSoup
-import urllib.request, urllib.parse, urllib.error
+import urllib.request
+import urllib.error
+import telebot
 
+bot = telebot.TeleBot('#')
+print('started')
+
+book_list = {}
+
+dialog_key = False
 
 def get_html(url): #get html file from url
     response = urllib.request.urlopen(url)
@@ -16,29 +23,28 @@ def parse(html, book_req=''): #parsing html to find book name and url
     key = 0
     soup = BeautifulSoup(html)
     rows = soup.findAll('li')
-    
     for row in rows:
         if str(row).find('sequence') == -1:
             #print(row)
             #print(str(row) + '\n')
-            line = row.find('span', text=book_req)
-            if line is not None: #TODO check 'sequence' tag
-                book_name = re.sub(r'<.*?>', '', str(row))
-                file_name = re.findall(r'/b/\d+', str(row.find('a')))
-                info = get_book_info(file_name[0])
-                files[key] = [book_name, file_name, get_book_info(file_name[0])]
-                key += 1
-                #print(book_name, file_name, info)
-            else:
-                line = row.find('b', text=book_req)
+            try:
+                line = row.find('span', text=book_req)
                 if line is not None:
                     book_name = re.sub(r'<.*?>', '', str(row))
                     file_name = re.findall(r'/b/\d+', str(row.find('a')))
                     info = get_book_info(file_name[0])
-                    files[key] = [book_name, file_name, get_book_info(file_name[0])]
+                    files[key] = [book_name, file_name, info]
                     key += 1
-                    #print(book_name, file_name, info)
-    #print(files)
+                else:
+                    line = row.find('b', text=book_req)
+                    if line is not None:
+                        book_name = re.sub(r'<.*?>', '', str(row))
+                        file_name = re.findall(r'/b/\d+', str(row.find('a')))
+                        info = get_book_info(file_name[0])
+                        files[key] = [book_name, file_name, info]
+                        key += 1
+            except:
+                continue
     return files
 
 def download(url, file_name): #download file from url to file
@@ -49,27 +55,57 @@ def get_book_info(url):
     info = soup.find('span', style="size").text
     return info
 
-def main():
-    while True:
-        book_req = input("enter book name\n")
+@bot.message_handler(commands=['start']) #if user send /help bot send back help
+def start_message(message):
+    global book_list, dialog_key
+    book_list = {}
+    dialog_key = False
+    bot.send_message(message.chat.id, 'hello')
+
+@bot.message_handler(content_types=['text']) #if user send text 
+def get_text_messages(message):
+    print(message.text)
+    main(message)
+    #send_message(message, message.chat.id)
+
+def main(message):
+    global dialog_key, book_list
+    if dialog_key == False:
+        book_list = {}
+        book_req = message.text
+
         url = 'http://flibusta.is/booksearch?ask={}'.format('+'.join(urllib.parse.quote(book_req).split(' ')))
         book_req = book_req.split()[0].capitalize()
         #print(book_req)
         book_list = parse(get_html(url), book_req)
         if book_list == {}:
             print('wrong book name')
-            continue
         for key, value in book_list.items():
             print(key, value[0], value[2])
         #for key, book in book_list: print(key,get_key book_list[book][0],  book_list[book][2] )
         #print(get_book_info('/b/21024'))
-        selected = int(input("enter selected book\n"))
-        #print(book_list[selected][1])
+        for key, value in book_list.items():
+            bot.send_message(message.chat.id, (str(key) + ' ' + value[0] + ' ' + value[2]))
+        dialog_key = True
+    else:
+        #print(book_list)
         try:
+            selected = int(message.text)
             download(book_list[selected][1][0], book_list[selected][0])
+            book = open(str('c:\\temp\\' + book_list[selected][0] +'.fb2'), 'rb')
+            bot.send_document(message.chat.id, book)
             print('done')
+            dialog_key = False
         except:
-            print('oops!')
+            bot.send_message(message.chat.id, 'try again')
+
+    """selected = int(input("enter selected book\n"))
+    #print(book_list[selected][1])
+    try:
+        download(book_list[selected][1][0], book_list[selected][0])
+        print('done')
+    except:
+        print('oops!')"""
 
 if __name__ == '__main__':
-    main()
+    bot.polling(none_stop = True)
