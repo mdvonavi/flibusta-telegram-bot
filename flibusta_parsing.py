5aa78195c5b26e8fd36233847083d9596139b767
mdvonavi <mdvonavi@gmail.com>
2020-01-29 17:11:13 +0300

    тестирование

--- flibusta_parsing.py
+++ flibusta_parsing.py
@@ -13,29 +13,36 @@ def get_html(url): #get html file from url
 
 def parse(html, book_req=''): #parsing html to find book name and url
     files = {}
+    key = 0
     soup = BeautifulSoup(html)
     rows = soup.findAll('li')
-
+    
     for row in rows:
-        #print(str(row) + '\n')
-        cols = row.find('span', text=book_req)
-        if cols is not None:
-            book_name = re.sub(r'<.*?>', '', str(row))
-            file_name = re.findall(r'/b/\d+', str(row.find('a')))
-            files[book_name] = file_name
-            print(book_name, get_book_info(file_name[0]))
-        else:
-            cols = row.find('b', text=book_req)
-            if cols is not None:
+        if row.find('sequence') == -1:
+            print(row)
+            #print(str(row) + '\n')
+            line = row.find('span', text=book_req)
+            if line is not None: #TODO check 'sequence' tag
                 book_name = re.sub(r'<.*?>', '', str(row))
                 file_name = re.findall(r'/b/\d+', str(row.find('a')))
-                files[book_name] = file_name
-                print(book_name, get_book_info(file_name[0]))
-
+                info = get_book_info(file_name[0])
+                #files[key] = [book_name, file_name, get_book_info(file_name[0])]
+                key += 1
+                print(book_name, file_name, info)
+            else:
+                line = row.find('b', text=book_req)
+                if line is not None:
+                    book_name = re.sub(r'<.*?>', '', str(row))
+                    file_name = re.findall(r'/b/\d+', str(row.find('a')))
+                    info = get_book_info(file_name[0])
+                    #files[key] = [book_name, file_name, get_book_info(file_name[0])]
+                    key += 1
+                    print(book_name, file_name, info)
+    print(files)
     return files
 
 def download(url, file_name): #download file from url to file
-    urllib.request.urlretrieve('http://flibusta.is' + url + '/fb2', 'c:\\temp\\file.fb2') #TOFO fix place to save
+    urllib.request.urlretrieve('http://flibusta.is' + url + '/fb2', 'c:\\temp\\file.fb2') #TODO fix place to save
 
 def get_book_info(url):
     soup = BeautifulSoup(get_html('http://flibusta.is' + url))
@@ -46,9 +53,9 @@ def main():
     book_req = input()
     url = 'http://flibusta.is/booksearch?ask={}'.format('+'.join(urllib.parse.quote(book_req).split(' ')))
     book_req = book_req.split()[0].capitalize()
-    print(book_req)
+    #print(book_req)
     book_list = parse(get_html(url), book_req)
-    for book in book_list: print(book) 
+    for book in book_list: print(book_list[book] )
     
     #print(get_book_info('/b/21024'))
 
